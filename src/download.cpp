101fef38488ad8fde2c22e46e5ed271252a8fada
urlordjames <urlordjames@gmail.com>
2021-03-18 17:44:29 -0400

    moved set curl options into constructor

--- src/download.cpp
+++ src/download.cpp
@@ -3,6 +3,16 @@
 Downloader::Downloader() {
 	curl_global_init(CURL_GLOBAL_ALL);
 	handle = curl_easy_init();
+
+	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, Downloader::writeFile);
+	curl_easy_setopt(handle, CURLOPT_FAILONERROR, 1);
+
+	// would be very bad for security if this was not set
+	curl_easy_setopt(handle, CURLOPT_USE_SSL, CURLUSESSL_ALL);
+
+#ifndef NDEBUG
+	curl_easy_setopt(handle, CURLOPT_VERBOSE, 1);
+#endif
 }
 
 Downloader::~Downloader() {
@@ -16,15 +26,6 @@ size_t Downloader::writeFile(void *file, size_t size, size_t nmemb, void *stream
 
 CURLcode Downloader::downloadFile(std::string url, std::filesystem::path path) {
 	curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
-	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, Downloader::writeFile);
-	curl_easy_setopt(handle, CURLOPT_FAILONERROR, 1);
-
-	// would be very bad for security if this was not set
-	curl_easy_setopt(handle, CURLOPT_USE_SSL, CURLUSESSL_ALL);
-
-#ifndef NDEBUG
-	curl_easy_setopt(handle, CURLOPT_VERBOSE, 1);
-#endif
 
 	// std::filesystem::path::c_str() doesn't work on Windows for some reason
 	FILE *file = fopen(path.string().c_str(), "wb");
